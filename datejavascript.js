582ba7a4c36068a630e6d0aa0afd040a71b84096
muhammad048 <45005981+muhammad048@users.noreply.github.com>
2021-05-29 19:24:31 +0600

    Add files via upload
    2nd update

---
+++ datejavascript.js
@@ -0,0 +1,8 @@
+// Data Picker Initialization
+$('.datepicker').datepicker();/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
